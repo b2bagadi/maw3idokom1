e464d4591030287ea405d242c50be55bd2f161ee
b2bagadi <b2bagadir.maroc@gmail.com>
2025-12-22 12:39:19 +0100

    commit

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import createMiddleware from 'next-intl/middleware';
-import { routing } from './src/i18n/routing';
-
-export default createMiddleware(routing);
-
-export const config = {
-  // Match all pathnames except for API routes, static files, and images
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
-};
